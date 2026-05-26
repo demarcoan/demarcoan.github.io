38bbed2e0ca8b84a491417cb1d0b8966578e7fd9
Andrea de Marco <anderni@hotmail.it>
2026-05-26 16:19:24 +0200

    Aggiorna sito

---
+++ script.js
@@ -0,0 +1,4 @@
+// Questo messaggio appare nella console del browser.
+// Serve solo per verificare che il file JavaScript sia collegato correttamente.
+
+console.log("The website has loaded correctly.");
