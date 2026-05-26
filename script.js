1de52be9b1347d346ed99531cb0687721faac61f
Andrea de Marco <anderni@hotmail.it>
2026-05-26 17:05:13 +0200

    footer

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 // Serve solo per verificare che il file JavaScript sia collegato correttamente.
 
 console.log("The website has loaded correctly.");
+
+document.querySelectorAll("[data-include]").forEach(element => {
+    const file = element.getAttribute("data-include");
+
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Errore nel caricamento di ${file}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            element.innerHTML = html;
+        })
+        .catch(error => console.error(error));
+});
